24dd8e663fa43037ac29c6502aed574d1203fcd7
offiongDavid <offiongd400@gmail.com>
2026-03-12 13:16:24 +0100

    gol

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use("/api/admin", adminRoutes);
 // Serve React build
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get(/^\/.*$/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
